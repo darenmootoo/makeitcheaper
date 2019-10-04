0fd1d9cca0c7dce34ec48c5712adee3aed77cb0f
Daren Mootoo <dmootoo@deloitte.co.uk>
2019-10-04 14:06:36 +0100

    TDD Front-end, based on acceptance criteria

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+it('renders without error', () => {
+
+})
+
+it('renders input field', () => {
+
+})
+
+it('renders input field, list of URLs', () => {
+
+})
+
+it('updates the Form state when then input field is changed', () => {
+
+})
+
+it('receives an object with shorturl and URL when POST to localhost:9000', () => {
+
+})
